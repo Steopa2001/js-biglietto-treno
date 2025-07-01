a540fb010be081b957d86bf3038b2e15f54c3025
Steopa2001 <moraru495@gmail.com>
2025-07-01 15:27:33 +0200

    feat: added prompt to user to ask for years

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,18 @@
-const user = prompt("Inserisci il numero di chilometri da percorrere");
-console.log(user);
+const kmInput = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+const km = (kmInput);
+console.log(km);
 
+const ageInput = parseInt(prompt("Quanti anni hai"));
+const age = (ageInput);
+console.log(age);
+
+const ticketPrice = km * 0.21;
+console.log(ticketPrice);
+
+if (eta < 18) {
+  sconto = 0.20; 
+} else if (eta > 65) {
+  sconto = 0.40; 
+} else {
+  sconto = 0;    
+}
